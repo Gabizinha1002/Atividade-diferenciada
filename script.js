52eaa7ab9928c394ff8a219bcdd3c4b95e665059
Gabizinha1002 <gabriella.marques.silva@escola.pr.gov.br>
2024-10-31 18:31:18 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 let perguntaAtual = 0;
 let perguntas = [
     {
-        codigo: `let x = 10;\nx += 5;\nx *= 2;\nconsole.log(x);`,
-        resposta: 30,
-        escolha: [15, 20, 25, 30]
+        codigo: `Em uma página web, temos dois parágrafos dentro de uma caixa. Quantos elementos de parágrafo existem dentro dessa caixa?\n\nExemplo de código:\nDiv - Início\n    Parágrafo - Primeiro parágrafo\n    Parágrafo - Segundo parágrafo\nDiv - Fim`,
+        resposta: 2,
+        opcoes: [1, 2, 3, 4]
     },
     {
-        codigo: `let y = 3;\ny -= 1;\ny **= 2;\nconsole.log(y);`,
-        resposta: 4,
-        escolha: [2, 4, 8, 9]
+        codigo: `Em um arquivo CSS, temos a seguinte regra:\n\n.caixa {\n    background-color: red;\n}\n\nNo HTML, aplicamos essa classe em um elemento Div:\n\n<div class="caixa"></div>\n\nQual será a cor de fundo exibida no elemento?`,
+        resposta: "vermelho",
+        opcoes: ["azul", "verde", "vermelho", "sem cor"]
     },
     {
-        codigo: `let a = 2;\nlet b = 5;\nlet c = a + b * 3;\nconsole.log(c);`,
-        resposta: 17,
-        escolha: [11, 15, 17, 21]
+        codigo: `Um loop em HTML/CSS imprime o valor de i de 0 a 2. Qual será a saída do programa?\n\nExemplo de código:\npara i em intervalo(3):\n    Imprimir(i)`,
+        resposta: "0 1 2",
+        opcoes: ["1", "2", "0 1 2", "Nenhuma saída"]
     }
 ];
 
 function exibirPergunta() {
     document.getElementById("codigo").innerText = perguntas[perguntaAtual].codigo;
-    let escolha = document.getElementById("escolha");
-    escolha.innerHTML = "";
-    perguntas[perguntaAtual].escolha.forEach(opcao => {
+    let opcoes = document.getElementById("opcoes");
+    opcoes.innerHTML = "";
+    perguntas[perguntaAtual].opcoes.forEach(opcao => {
         let botao = document.createElement("button");
         botao.innerText = opcao;
         botao.onclick = () => verificarResposta(opcao);
-        escolha.appendChild(botao);
+        opcoes.appendChild(botao);
     });
     document.getElementById("feedback").innerText = "";
     document.getElementById("proximo-botao").style.display = "none";
